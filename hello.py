ec620e97a777d4a24f85bc875978043d70de7605
awstine <awstinejessy@gmail.com>
2023-12-27 10:22:15 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+def main():
+    name = input("What is your name? ")
+    print("Hello, " + name + "!")
+    print("Nice to meet you!",name)
+    print("Goodbye,",name)
+    
+if __name__ == "__main__":
+    main()
---
+++ variables.py
@@ -0,0 +1,16 @@
+myInt = 5
+myStr = "Hello"
+myFloat = 3.14159
+myBool = True
+myList = [1,2,3,4,5]
+myTuple = (1,2,3,4,5)
+myDict = {"name":"John","age":30,"city":"New York"}
+mySet = {1,2,3,4,5}
+print(myInt)
+print(myStr)
+print(myFloat)
+print(myBool)
+print(myList)
+print(myTuple)
+print(myDict)
+print(mySet)

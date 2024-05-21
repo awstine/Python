c5d90c10cb02b512551a54a5b84d3cdf7cd0b510
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-05-21 08:32:45 -0700

    initial commit

--- fuctions1.py
+++ fuctions1.py
@@ -1,10 +1,19 @@
-from math import *
+#Always start with a def
 
-print(30 * 24)
+def person_1(name):
+    print(name+ "how can I i help you")
 
-one = 23
-print("43", + one)
+def person_2(food,desert,drink,name):
+    name = input("What is your name: ")
+    food = input("what food do u wanna eat ")
+    drink = input("What drink do u want ")
+    desert = input("What desert do you want ")
 
-print(2, 4, 5, 3)
-print(sqrt(9357))
+    print(name+ " took",food, "and",drink, "then finished with" +desert )
 
+def person_3(name):
+    print(name+ "Thank you")
+
+person_1("Cashier")
+person_2("food","desert","drink","name")
+person_3("Cashier")

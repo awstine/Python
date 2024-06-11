7148653ca35a400c98d9a7a54b8af06f58cb22f4
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-06-11 11:50:19 -0700

    initial commit

---
+++ pizza/classProject.py
@@ -0,0 +1,6 @@
+from classes import pizza1
+
+pizzav = pizza1("Vegeterain", 5, "Small")
+pizzap = pizza1("Meterian", 8, "Big")
+
+print(pizzav.size)
---
+++ pizza/classes.py
@@ -0,0 +1,8 @@
+#Classes are set of data that help crate another same type of data that is personalised
+
+
+class pizza1:
+    def __init__(self, type,price,size):
+        self.type = type
+        self.price = price
+        self.size = size

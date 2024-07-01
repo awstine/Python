d5ba2e701db755411ed7aef12362976b24bc6ad6
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-07-01 10:48:05 -0700

    initial commit

---
+++ Python2/dictonary.py
@@ -0,0 +1,2 @@
+player_number = {"messi":10, "ronaldo": 10,"garnacho":17,"Bruno":18}
+print(player_number)
---
+++ Python2/list.py
@@ -0,0 +1,9 @@
+boston = ['tatum', 'brown', 'pozingis', 'holiday', 'white']
+print(boston)
+mixed = ["Hey", [4, 5, 6], 5, 'roro']
+print(len(mixed))
+
+#Tuples
+city = ("Bungoma", "Nairobi", "Kisumu")
+city2 = ("Mombasa", "Nyeri",)  #coma shows its a tuple
+print(city2)

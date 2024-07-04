53fbf8566e3e03553af8229f84a2aa5a931ae43e
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-07-04 04:28:38 -0700

    initial commit

--- Python2/dictonary.py
+++ Python2/dictonary.py
@@ -1,2 +1,5 @@
 player_number = {"messi":10, "ronaldo": 10,"garnacho":17,"Bruno":18}
 print(player_number)
+
+player_number1 = dict({"messi":10, "ronaldo": 10,"garnacho":17,"Bruno":18})
+print(type(player_number1))

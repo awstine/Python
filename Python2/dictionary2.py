50514d79cf6fa7ab5ae29110c4e811c2776e95fe
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-10-13 23:45:16 -0700

    initial commit

---
+++ Python2/dictionary2.py
@@ -0,0 +1,10 @@
+fruits = {"Apple": 5, "banana": 6, "guava": 3}
+print(fruits.get("banana"))
+
+fruits2 = {"Apple": [1, 2, 3], "banana": 6, "guava": 3}
+print(fruits2["Apple"][1])
+
+#UPDATE,ADD, DEL A DICTIONARY
+planet = {"Roro":7}
+planet["Roro"] = 11
+print(planet)

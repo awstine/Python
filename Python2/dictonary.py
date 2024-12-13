0c1d4e8b58c6586e4918f14cf6322cf2bd447f8b
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-12-12 20:03:54 -0800

    initial commit

--- Python2/dictonary.py
+++ Python2/dictonary.py
@@ -1,5 +1,1 @@
-player_number = {"messi":10, "ronaldo": 10,"garnacho":17,"Bruno":18}
-print(player_number)
-
-player_number1 = dict({"messi":10, "ronaldo": 10,"garnacho":17,"Bruno":18})
-print(type(player_number1))
+Hello world
---
+++ plots.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 6, 3], [4, 5, 3])
+
+plt.title("Title")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
